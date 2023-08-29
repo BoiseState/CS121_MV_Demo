9e35f803b89092e29d4e16ee5200e9edbcf2af3b
mvail <masonvail@boisestate.edu>
2023-08-29 15:58:20 -0600

    Added demo for primitives

---
+++ PrimitivesDemo.java
@@ -0,0 +1,22 @@
+public class PrimitivesDemo {
+    public static void main(String[] args) {
+        int firstInt = 10;
+        int secondInt = 3;
+
+        int sum = firstInt + secondInt;
+
+        System.out.println(firstInt + " + " + secondInt + " = " + sum);
+
+        //int badPi = 3.14;
+
+        double goodPi = 3.14;
+
+        //boolean notValid = "true";
+
+        boolean isValid = true;
+
+        System.out.println(firstInt + " / " + secondInt + " = " + (firstInt / secondInt));
+        System.out.println(firstInt + " % " + secondInt + " = " + (firstInt % secondInt));
+
+    }
+}
