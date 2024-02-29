989f89dcfe50170ad039fe32053c827426643d90
MasonVail <masonvail@boisestate.edu>
2024-02-29 07:45:29 -0700

    going loopy

--- ConditionalsAndLoops/LoopDemo.java
+++ ConditionalsAndLoops/LoopDemo.java
@@ -6,6 +6,16 @@ public class LoopDemo {
     public static void main(String[] args) {
         int evenSum = 0;
         int oddSum = 0;
+        for (int i = 10; i <= 100; i+=2) {
+            evenSum += i;
+            if(i != 100) {
+                oddSum += i+1;
+            }
+        }
+        System.out.println("evenSum: " + evenSum);
+        System.out.println("oddSum: " + oddSum);
+        // int evenSum = 0;
+        // int oddSum = 0;
 
         // for (int i = 10; i <= 100; i++) {
         //     if (i % 2 == 0) {
@@ -26,15 +36,15 @@ public class LoopDemo {
     //         }
     //     }
         //or
-        for (int i = 10; i < 101; i+=2) {
-            evenSum += i;
-        }
-        for (int i = 11; i < 101; i+=2) {
-            oddSum += i;
-        }
+        // for (int i = 10; i < 101; i+=2) {
+        //     evenSum += i;
+        // }
+        // for (int i = 11; i < 101; i+=2) {
+        //     oddSum += i;
+        // }
 
-        System.out.println("EvenSum: " + evenSum);
-        System.out.println("OddSum: " + oddSum);
+        // System.out.println("EvenSum: " + evenSum);
+        // System.out.println("OddSum: " + oddSum);
 
         String input = "A string";
         for (int i = 0; i < input.length(); i++) {
@@ -43,12 +53,12 @@ public class LoopDemo {
 
         String word = "spoons";
         String reverse = "";
-        // for (int i = word.length()-1; i > -1; i--) {
-        //     reverse = reverse + word.charAt(i);
-        // }
-        for (int i = 0; i < word.length(); i++) {
-            reverse = word.charAt(i) + reverse;
+        for (int i = word.length()-1; i > -1; i--) {
+            reverse = reverse + word.charAt(i);
         }
+        // for (int i = 0; i < word.length(); i++) {
+        //     reverse = word.charAt(i) + reverse;
+        // }
         System.out.println(word + " reversed is " + reverse);
     }
 }
